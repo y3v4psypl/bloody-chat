e31cf3c960e19767e7f157524e0d05eebef5cecb
Eva Vetrova <yevavetrova@gmail.com>
2022-08-11 13:08:52 +0300

    Update Header

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,23 +1,38 @@
 import * as React from "react";
 import {BiChat} from "react-icons/bi";
 import styles from "./Header.module.scss"
+import Link from 'next/link';
 
 export const Header = () => {
     return (
         <div className={styles.header}>
-            <div className={styles.link}>
-                <a href="/">
-                    <div className={styles.logo}> <BiChat /> </div>
-                    <div className={styles.heading}> <h1>BloodyChat</h1> </div>
-                </a>
+            <div className={styles.logoLink}>
+                <Link  href="/">
+                    <a tabIndex={1} className={styles.logo}>
+                        <span className={styles.logoImage}><BiChat/></span>
+                        <span className={styles.logoName}><h1>BloodyChat</h1></span>
+                    </a>
+                </Link>
             </div>
 
             <nav className={styles.navbar}>
-                <ul>
-                    <a className={styles.link} href="/chats"><li>Chats</li></a>
-                    <a className={styles.link} href="/signup"><li>Sign Up</li></a>
-                    <a className={styles.link} href="/about"><li>About</li></a>
-                </ul>
+                {/*<ul>*/}
+                    <Link href="/chats">
+                        {/*<li className={styles.link}>*/}
+                            <a className={styles.link} tabIndex={2}>Chats</a>
+                        {/*</li>*/}
+                    </Link>
+                    <Link className={styles.link} href="/signup">
+                        {/*<li className={styles.link}>*/}
+                            <a className={styles.link} tabIndex={3}>Sign Up</a>
+                        {/*</li>*/}
+                    </Link>
+                    <Link href="/about">
+                        {/*<li >*/}
+                            <a className={styles.link} tabIndex={4}>About</a>
+                        {/*</li>*/}
+                    </Link>
+                {/*</ul>*/}
             </nav>
         </div>
     );
