348842c3ee1d6f3c81f9c6806fec657be08caefe
Eva Vetrova <yevavetrova@gmail.com>
2022-08-11 13:12:34 +0300

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,18 +5,20 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
 function App({ Component, pageProps, router }: AppProps): JSX.Element {
-  return <>
-    <Head>
-      <title>Bloody Chat</title>
+  return (
+    <>
+      <Head>
+        <title>Bloody Chat</title>
 
-      <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAOH/AP8AewD/hAAAAP8qAKYPFgAAAP8ABQUFAABV/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACIiIiIiIiIAIiIiIiIiIgBVVVVVVVVVB1VVVVVVVVUAMzMzMzMzMwAzMzMzMzMzAEREREREREQARERERERERAARERERERERABEREREREREAiIiIiIiIiACIiIiIiIiIAGZmZmZmZmYAZmZmZmZmZgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-            rel="icon"/>
+        <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAOH/AP8AewD/hAAAAP8qAKYPFgAAAP8ABQUFAABV/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACIiIiIiIiIAIiIiIiIiIgBVVVVVVVVVB1VVVVVVVVUAMzMzMzMzMwAzMzMzMzMzAEREREREREQARERERERERAARERERERERABEREREREREAiIiIiIiIiACIiIiIiIiIAGZmZmZmZmYAZmZmZmZmZgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+              rel="icon"/>
 
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' />
-    </Head>
-    <Component {...pageProps} />
-  </>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App
